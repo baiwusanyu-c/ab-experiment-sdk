3c97d79776aec5a9f84ca5e6ff10b2cc89f4a8cd
baiwusanyu <740132583@qq.com>
2022-09-08 13:43:04 +0800

    update: 自动刷新日志输出修改

--- play/src/main.tsx
+++ play/src/main.tsx
@@ -4,16 +4,18 @@ import App from './App'
 import './index.css'
 
 // @ts-ignore
-/*import { ABTest } from '../../dist/web/esm/ab-test-sdk-web.esm'
+import { ABTest } from '../../dist/web/esm/ab-test-sdk-web.esm'
 ABTest('init', {
   appKey: 'SDK_TEST_WB_APP', // 替换成您的appKey
   log: true, // 是否打印log
+  autoRefresh:false,
+  autoRefreshStep:500,
    userId:'GACo74wkDIkDzEhkwRwgjGt1pqlk',
   //userId:'GACo74wkDIkDzEhkwRwgjGz1123',
 })
 
-await ABTest('start')*/
+await ABTest('start')
 
-import './main-process-test'
+// import './main-process-test'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
