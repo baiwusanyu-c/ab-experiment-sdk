3c96bc05cf8634af5843fb411164b4660b820ca7
baiwusanyu <740132583@qq.com>
2022-09-08 11:49:06 +0800

    update: 神策主流程测试小程序脚本修改，并发接口调用模拟

--- play-miniwechat/main-process-test.js
+++ play-miniwechat/main-process-test.js
@@ -21,19 +21,30 @@ const callABTest = async (length) =>{
     })
 }
 
-async function run(length, arr) {
-    for(let i = 0;i < length; i++){
-        // 初始化sdk，并分流
-        const res = await callABTest(i)
-        console.log(`%c ${((i / length) * 100).toFixed()}%`,'color:#4AB7BD;font-size:25px')
-        if(!res) continue
-        arr.push(res)
+async function run(length, arr,concurrent) {
+    // promise.all 并发调用
+    if(concurrent){
+        let arrList = []
+        for(let i = 0;i < length; i++){
+            arrList.push(callABTest(i))
+        }
+        console.log(`%c ABTest获取实验参数接口并发调用中...`,'color:#4AB7BD;font-size:25px')
+        arr = await Promise.all(arrList)
+        arr = [...new Set(arr)].filter((val)=>val)
+    }else{
+        for(let i = 0;i < length; i++){
+            // 初始化sdk，并分流
+            const res = await callABTest(i)
+            console.log(`%c ${((i / length) * 100).toFixed()}%`,'color:#4AB7BD;font-size:25px')
+            if(!res) continue
+            arr.push(res)
+        }
     }
     return arr
 }
 
-export async function testResult(num) {
-    let res = await run(num, []);
+export async function testResult(num,concurrent=true) {
+    let res = await run(num, [],concurrent);
     return res
 }
 
--- play-miniwechat/pages/home/home.js
+++ play-miniwechat/pages/home/home.js
@@ -5,6 +5,7 @@ Page({
     progress:0
   },
   async onInitABTest(){
+    let cur = Number((new Date()))
     app.abtest = await testResult(1000)
     const logArr = app.abtest.map(val=>{
       return {userId: val.sdkKey}
@@ -12,6 +13,7 @@ Page({
     console.log(`%c 分流进入实验用户数：${logArr.length}`,'color:#4AB7BD;font-size:20px')
     console.table(logArr)
     console.log('%c abtest 初始化模拟分流完成...','color:#30B08F;font-size:25px')
+    console.log(`%c 接口调用耗时:${Number(new Date()) - cur}`,'color:#30B08F;font-size:25px')
   },
   onButtonTap(){
     wx.navigateTo({
