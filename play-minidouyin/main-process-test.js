5618702307873bf34c88a00e482ce9a9ecac1191
baiwusanyu <740132583@qq.com>
2022-09-09 11:21:11 +0800

    chore: 抖音小程序play 编写

---
+++ play-minidouyin/main-process-test.js
@@ -0,0 +1,50 @@
+
+const callABTest = async (length) =>{
+    return new Promise(async (resolve, reject) => {
+        const { ABTest } = await (import('./ab-test-sdk-mini-douyin.esm'))
+
+        const sdkKey = `GACo74wkSCABTESTDIkDzEhkwRwgjGt1pqlk${length}`
+        ABTest({funcName:'init',sdkKey}, {
+            appKey: 'SC_TEST_APP', // 替换成您的appKey
+            log: false, // 是否打印log
+            userId:sdkKey,
+            //userId:'GACo74wkDIkDzEhkwRwgjGz1123',
+        })
+
+        const sdk = await ABTest({funcName:'start',sdkKey})
+        // 只返回进入实验的
+        if(sdk.res.shuntRes[sdk.res.expConfig[0].experimentId].isEntry){
+            resolve({ABTest,sdk,sdkKey:sdkKey})
+        }else{
+            resolve(null)
+        }
+    })
+}
+
+async function run(length, arr,concurrent) {
+    // promise.all 并发调用
+    if(concurrent){
+        let arrList = []
+        for(let i = 0;i < length; i++){
+            arrList.push(callABTest(i))
+        }
+        console.log(`%c ABTest获取实验参数接口并发调用中...`,'color:#4AB7BD;font-size:25px')
+        arr = await Promise.all(arrList)
+        arr = [...new Set(arr)].filter((val)=>val)
+    }else{
+        for(let i = 0;i < length; i++){
+            // 初始化sdk，并分流
+            const res = await callABTest(i)
+            console.log(`%c ${((i / length) * 100).toFixed()}%`,'color:#4AB7BD;font-size:25px')
+            if(!res) continue
+            arr.push(res)
+        }
+    }
+    return arr
+}
+
+export async function testResult(num,concurrent=true) {
+    let res = await run(num, [],concurrent);
+    return res
+}
+
---
+++ play-minidouyin/pages/home/home.js
@@ -0,0 +1,24 @@
+import {testResult} from '../../main-process-test.js'
+const app = getApp()
+
+Page({
+  data: {
+    progress:0
+  },
+  async onInitABTest(){
+    let cur = Number((new Date()))
+    app.abtest = await testResult(10)
+    const logArr = app.abtest.map(val=>{
+      return {userId: val.sdkKey}
+    })
+    console.log(`%c 分流进入实验用户数：${logArr.length}`,'color:#4AB7BD;font-size:20px')
+    console.table(logArr)
+    console.log('%c abtest 初始化模拟分流完成...','color:#30B08F;font-size:25px')
+    console.log(`%c 接口调用耗时:${Number(new Date()) - cur}`,'color:#30B08F;font-size:25px')
+  },
+  onButtonTap(){
+    tt.navigateTo({
+      url: `/pages/demo/demo`,
+    })
+  },
+})
